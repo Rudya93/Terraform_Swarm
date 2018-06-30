5931bf6aacf79a2f4bc92db54acd5f7cd571940d
Rudya93 <rudyaa93@gmail.com>
2018-06-30 13:18:26 +0300

    Update out.tf

--- out.tf
+++ out.tf
@@ -2,9 +2,7 @@ output "master.ip" {
   value = "${aws_instance.master.public_ip}"
 }
 
-output "swarm0.ip" {
-  value = "${aws_instance.slave0.public_ip}"
+output "swarm.ip" {
+  -  value = "${aws_instance.slave.*.public_ip}"
 }
-output "swarm1.ip" {
-  value = "${aws_instance.slave1.public_ip}"
-}
+
